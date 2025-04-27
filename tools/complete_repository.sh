b5ed0b47766ed8110d794ad1036481dd780caca3
Martin Mauersics <revision@maus-games.at>
2025-04-27 16:01:32 +0200

    changed default folder naming convention from PascalCase to dash-case (as this is the naming convention for all repositories)

--- tools/complete_repository.sh
+++ tools/complete_repository.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../WindEngine/tools/scripts/download_archive.sh keep-going music "../executable/data/music"
+../../wind-engine/tools/scripts/download_archive.sh keep-going music "../executable/data/music"
